bd3899e779d02c85af4045baa6fda2c0d2347397
Mgreat01 <ephraimmonga5@gmail.com>
2026-04-16 14:40:00 +0200

    refactor : mise a  jour de la logique de connexion au bd

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost/secure_alert"
+from app.core.config import settings
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -13,3 +12,10 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
