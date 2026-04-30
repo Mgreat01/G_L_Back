49128e2c7d89980f4ed675f0760c7af2d274641b
Mgreat01 <ephraimmonga5@gmail.com>
2026-04-30 13:19:42 +0200

    feat : implementation de la gstion des routes api

---
+++ backend/app/routes/alert_routes.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+
+from app.schemas.alert_schema import (
+    AlertCreate,
+    AlertUpdate,
+    AlertResponse
+)
+
+from app.services.alert_service import AlertService
+
+router = APIRouter(
+    prefix="/alerts",
+    tags=["Alerts"]
+)
+
+@router.post("", response_model=AlertResponse)
+def create_alert(
+    payload: AlertCreate,
+    db: Session = Depends(get_db)
+):
+
+    user_id = "550e8400-e29b-41d4-a716-446655440000"
+
+    return AlertService.create_alert(
+        db,
+        payload,
+        user_id
+    )
+
+@router.get("", response_model=list[AlertResponse])
+def get_alerts(
+    db: Session = Depends(get_db)
+):
+
+    return AlertService.get_alerts(db)
+
+@router.put("/{alert_id}")
+def update_alert(
+    alert_id: str,
+    payload: AlertUpdate,
+    db: Session = Depends(get_db)
+):
+
+    alert = AlertService.update_alert(
+        db,
+        alert_id,
+        payload
+    )
+
+    if not alert:
+        raise HTTPException(
+            status_code=404,
+            detail="Alert not found"
+        )
+
+    return alert
