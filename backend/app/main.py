c8915294c218207f28b8e4d8127b6cc197b8f3ca
Mgreat01 <ephraimmonga5@gmail.com>
2026-05-05 14:27:39 +0200

    feat : ajout de la gestion de la creation de table

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from app.routes.alert_routes import router as alert_router
+from app.core.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Secure Alert Platform"
